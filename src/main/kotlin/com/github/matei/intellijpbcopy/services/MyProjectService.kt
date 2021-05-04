e08b53f32ab586ffbdb8d1843002c04c00a0b8ca
GitHub Action <action@github.com>
2021-05-04 14:02:53 +0000

    Template cleanup

--- src/main/kotlin/com/github/matei/intellijpbcopy/services/MyProjectService.kt
+++ src/main/kotlin/com/github/matei/intellijpbcopy/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.matei.intellijpbcopy.services
 
+import com.github.matei.intellijpbcopy.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
